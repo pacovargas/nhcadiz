cc25bf0102a06149cc850265f0f4b6db7d6fdeb9
Francisco Vargas <francisco.vargas@lineagrafica.es>
2023-01-27 18:16:35 +0100

    smarty básico funcionando

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require('./libs/smarty/Smarty.class.php');
+$smarty = new Smarty();
+
+$smarty->setTemplateDir('./smarty/templates');
+$smarty->setCompileDir('./smarty/templates_c');
+$smarty->setCacheDir('./smarty/cache');
+$smarty->setConfigDir('./smarty/configs');
+
+$smarty->assign('name', 'Ned');
+$smarty->display('index.tpl');
---
+++ smarty/templates_c/569f7c9bad1e95dec6a51c7c51b98d4db367f54c_0.file.index.tpl.php
@@ -0,0 +1,33 @@
+<?php
+/* Smarty version 4.3.0, created on 2023-01-27 17:16:02
+  from '/var/www/html/smarty/templates/index.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.0',
+  'unifunc' => 'content_63d406d2b0d085_60838502',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '569f7c9bad1e95dec6a51c7c51b98d4db367f54c' => 
+    array (
+      0 => '/var/www/html/smarty/templates/index.tpl',
+      1 => 1674839755,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_63d406d2b0d085_60838502 (Smarty_Internal_Template $_smarty_tpl) {
+?><html>
+  <head>
+    <title>Smarty</title>
+  </head>
+  <body>
+    Hello, <?php echo $_smarty_tpl->tpl_vars['name']->value;?>
+!
+  </body>
+</html><?php }
+}
